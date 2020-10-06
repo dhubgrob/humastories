5aaabc9a5e5980376b16667299512241ce0b1cae
Nicolas Salvi <dhubgrob@protonmail.com>
2020-10-06 15:03:20 +0200

    forst commit

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
+define('DB_NAME', 'humastories');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', 'http://localhost/humastories');
+// Site Name
+define('SITENAME', 'humastories');
+// App Version
+define('APPVERSION', '1.0.0');
